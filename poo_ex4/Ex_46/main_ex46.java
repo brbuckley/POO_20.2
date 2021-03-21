130a607a86dbf9a2e71f5ef338535ebfec976f44
Fafnir-D3 <78387169+Fafnir-D3@users.noreply.github.com>
2021-03-21 20:02:20 -0300

    Exercicio 46 versao 2
    Atualizacao do exercicio 46

--- poo_ex4/Ex_46/main_ex46.java
+++ poo_ex4/Ex_46/main_ex46.java
@@ -4,21 +4,22 @@ import java.util.ArrayList;
 
 /*
 Deixei todas as classes nesse mesmo arquivo por facilidade de leitura, mas 
-faria um arquivo para cada numa situacao real. Nao consegui pensar em formas de 
-heranca, na minha cabeca essas classes nao funcionariam bem como extensao de 
-outras. E as possiveis extensoes para as classes ja criadas me parecem apenas 
-instancias diferentes da mesma classe. Talvez Pikachu ou Charizard pudessem ser
-subclasses de pokemon, mas ao ler o problema eu entendi que seriam instancias e 
-nao consegui pensar em muitos beneficios para que fossem subclasses.
+faria um arquivo para cada numa situacao real. Pensei no uso de herencas entre
+um pokemon generico e um pokemon de um elemento, entre o pokemon de um elemento
+e o "tipo" de pokemon como Pikachu ser um PokeRaio que eh um Pokemon. Alem disso
+achei interessante a ideia de tentar manter a instancia e fazer um Raichu ser 
+subclasse de Pikachu, mas nao sei se funcionaria como eu espero. Outro lugar que
+usei heranca foi em Ataques, para cada tipo de Ataque; em Condicoes para cada
+tipo de Condicao; e em Habilidade.
+
+Nao implementei todas as funcoes e todos os construtores, tentei deixar o mais 
+basico possivel focando apenas em heranca e com o minimo de complexidade para 
+fazer um exemplo de um pikachu do terinador Ash atacando um Charizard do 
+treinador Red e evoluindo com a experiencia.
 */
 
-/*Pensei em extender Pokemon de Tipo, mas acho que causaria mais problemas que
-solucoes. Dessa forma ele cria uma copia do tipo que pode ser modificada caso 
-evolua ou sofra algum modificador de stat.
-*/
 public class Pokemon{
     
-    Tipo tipo=new Tipo(); //guarda os stats base, e pode ser modificado
     String nome; //nome da "raca" do pokemon, como "Pikachu"
     String aparência, apelido;
     int tamanho, peso;
@@ -27,6 +28,8 @@ public class Pokemon{
     
     int HPatual; //contabiliza o HP atual caso seja ferido, diferente do meuHP que contabilizao HP total
     
+    Habilidade habilidade;
+    Ataque ataques[]= new Ataque[4];
     Condicao condicao;
     ArrayList<Modificador> modificadores; //guarda os modificadores de stats ou efeitos de ataques temporarios
     
@@ -36,18 +39,46 @@ public class Pokemon{
     
 }
 
-public class Tipo{
+public class PokeEletrico extends Pokemon{
     
-    String elemento; //nome do elemento
-    int HP, Atk, Def, SpAtk, SpDef, Spd; // stats base
+    int HP=64, Atk=74, Def=69, SpAtk=86, SpDef=71, Spd=80; // stats base
     String ataques[]; //guarda o nome de todos os ataques que um pokemon desse tipo pode aprender
     
 }
 
-public class Ataque{
+public class Pikachu extends PokeEletrico{
     
-    String nome, descricao;
+    String habilidades[]; //guarda as possiveis habilidades que um pikachu pode ter
+    public Pikachu(){this.Atk+=10;} //modifica os stats base para serem os stats unicos do pikachu, usei apenas 1 para servir de exemplo
+    
+}
+
+public class Raichu extends Pikachu{
+    
+    String novasHabilidades[];
+    void atualizarStats(){}; //caso o pokemon seja um pikachu que evoluiu
+    void atualizarAparencia(){};
+    
+}
+
+public class PokeFogo extens Pokemon{}
+
+public class Charmander extends Pokemon{}
+
+public class Charmeleon extends Charmander{}
+
+public class Charizard extends Charmeleon{}
+
+public class Ataque{
+
+    String descricao;
     int precisão, potência, quantidade;
+    
+}
+
+public class ChoqueTrovao extends Ataque{
+    
+    CondicaoParalizar paralizar; //talvez seja static, dai seria implementado de outra forma
     
 }
 
@@ -66,31 +97,46 @@ public class Treinador{
 
 public class Habilidade{
     
-    String nome, descricao;
-    Condicao condicao;
-    Modificador modificador;
+    String descricao;
+    
+}
+
+public class Estatica extends Habilidade{
+    
+    CondicaoParalizar estatica;
     
 }
 
 public class Condicao{
     
-    String nome, descricao;
-    //implementar efeito
+    String descricao;
+    int precisão;
+}
+
+public class CondicaoParalizar extends Condicao{
+    
+    void efeito(){}//implementar efeito talvez seja static
+    
 }
 
 public class Modificador{//"buff/debuff" aumento ou diminuicao de stat temporario e descricao
     
     String nome, descricao;
-    int stat1, stat2;//pode afetar ate 2 stats
+    int stat, turnos;
     
 }
 
 public class Batalha{//achei relevante uma classe batalha para nao sobrecarregar a main
     
     Treinador treinador1,treinador2;
+    int contaTurnos; //atualizador de turnos
+    Pokemon pokemon1, pokemon2;
     
     ArrayList<Modificador> modificadores; //efeitos que podem ocorrer no campo
     ArrayList<Condicao> condicoes;
+    
+    //teriam mais parametros, mas botei esses 2 para evitar um erro na main
+    public Batalha(Treinador treinador1, Treinador treinador2){}
     
 }
 
@@ -98,24 +144,21 @@ public class main_ex46 {
     
     public static void main(String[]args){
         
-        Pokemon pikachu=new Pokemon();
-        Tipo raio=new Tipo();
+        Pokemon meuPikachu=new Pikachu();
         Treinador Ash=new Treinador();
         Treinador Red=new Treinador();
-        Ataque choqueTrovao=new Ataque();
-        Pokemon charizard=new Pokemon();
-        Batalha batalha=new Batalha();
-        Condicao paralizado=new Condicao();
-        Habilidade estatica=new Habilidade();
+        Pokemon meuCharizard=new Charizard();
+        Batalha batalha=new Batalha(Ash,Red);
         
         /*provavelmente seria feito de outra forma, mas tentei ilustrar de maneira
         simples um treinador enviando um pokemon, que ataca outro e inflinge uma
         condicao. Apos a batalha o pokemon evolui.
         */
-        Ash.enviarPokemon(pikachu);
-        pikachu.atacar(choqueTrovao);
-        charizard.condicao=paralizado;
-        pikachu.evoluir();
+        batalha.treinador1.enviarPokemon(meuPikachu);
+        batalha.treinador2.enviarPokemon(meuCharizard);
+        batalha.pokemon1.atacar(batalha.pokemon1.ataques[0]); //selecionando o primeiro ataque dos 4 que meu pikachu tem
+        meuPikachu.evoluir(); //a ideia eh implementar uma evolucao que mantenha 
+        //a instancia e "promova" ela a uma subclasse, mas nao sei se funcionaria.
     }
     
 }
